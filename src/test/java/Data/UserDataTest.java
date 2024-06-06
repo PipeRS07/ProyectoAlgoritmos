fba631baf429739966862d83836276ff34b6c088
PipeRS07 <p2003rs0707@gmail.com>
2024-06-05 20:27:25 -0600

    Avance de registro de usuarios y busqueda de usuarios por nombre y contraseña

---
+++ src/test/java/Data/UserDataTest.java
@@ -0,0 +1,32 @@
+package Data;
+
+import domain.clasesBase.Administrador;
+import domain.clasesBase.Instructor;
+import org.junit.jupiter.api.Test;
+import util.Ruta;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserDataTest {
+    @Test
+    void test1(){
+        UserData data;
+            try {
+                data = new UserData();
+                data.agregarAlFinal( new Administrador(21, "Nigel",  "email", Ruta.USUADMIN, util.Encriptacion.obtenerContraseniaCifrada("123")));
+                data.agregarAlFinal(new Instructor(31, "Veroa", "@gmailgg", Ruta.USUINSTRUCTOR, "hola123"));
+                System.out.println(data.getUsuarioPorNombre("Nigelaaaa"));
+                System.out.println(data.getUsuarioPorNombre("Veroa"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+
+    }
+
+
+
+}
