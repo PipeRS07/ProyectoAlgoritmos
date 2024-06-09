a8c460812176ec281a3de029e5ece56c23d5db57
Algoritmos <veronicaagueroaguilar@gmail.com>
2024-06-08 20:56:45 -0600

    Interfaces Segundo avance completas

--- src/main/java/util/Fabrica.java
+++ src/main/java/util/Fabrica.java
@@ -9,10 +9,32 @@ import java.time.LocalDate;
 
 public class Fabrica {
 
+
+
+    public static User fabricaUsuarios(int id, String name, String email, String role, String contrasenia){
+        if(role.equals(Ruta.USUADMIN)){
+            return new Administrador(id, name, email, role, contrasenia);
+        }
+        if(role.equals(Ruta.USUINSTRUCTOR)){
+            return new Instructor(id, name, email, role, contrasenia);
+        }
+        if(role.equals(Ruta.USUESTUDIANTE)){
+            return new Estudiante(id, name, email, role, contrasenia);
+        }
+        return null;
+    }
+
+    public static void fabricaTDA(Object usuariosEnElSistema, User user) {
+        if(usuariosEnElSistema instanceof CircularDoublyLinkedList){
+            ((CircularDoublyLinkedList) usuariosEnElSistema).add(user);
+        }
+
+    }
+
     public static User fabricaUsuarios(User usuario){
-       if(usuario.getRole().equals(Ruta.USUADMIN)){
-           return new Administrador(usuario.getId(), usuario.getName(), usuario.getEmail(), usuario.getRole(), usuario.getContrasenia());
-       }
+        if(usuario.getRole().equals(Ruta.USUADMIN)){
+            return new Administrador(usuario.getId(), usuario.getName(), usuario.getEmail(), usuario.getRole(), usuario.getContrasenia());
+        }
         if(usuario.getRole().equals(Ruta.USUINSTRUCTOR)){
             return new Instructor(usuario.getId(), usuario.getName(), usuario.getEmail(), usuario.getRole(), usuario.getContrasenia());
         }
